8c2574395d4e4cff7f1b59f39fdb621ba391b10a
Priya-hub-wq <priyanka.dhakate24@pccoepune.org>
2026-09-04 11:44:16 +0530

    Add output for ambulance.cpp execution

---
+++ output of ambulance.cpp
@@ -0,0 +1,30 @@
+Enter number of intersections (vertices): 6
+Enter number of roads (edges): 7
+Enter edges (u v w):
+0 1 4
+0 2 2
+1 2 1
+1 3 5
+2 3 8
+2 4 10
+3 5 2
+Enter ambulance start location (source): 0
+Enter number of hospitals: 2
+Enter hospital nodes: 3 5
+Nearest hospital is at node 3 with travel time 10 minutes.
+
+Enter number of intersections (vertices): 7
+Enter number of roads (edges): 8
+Enter edges (u v w):
+0 1 3
+0 2 5
+1 2 2
+1 3 6
+2 4 4
+3 4 1
+3 5 2
+4 6 3
+Enter ambulance start location (source): 0
+Enter number of hospitals: 3
+Enter hospital nodes: 3 5 6
+Nearest hospital is at node 3 with travel time 9 minutes.
